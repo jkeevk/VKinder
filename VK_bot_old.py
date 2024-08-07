2b302a7e80415692d2edc734bee55cf49cfcec43
Magic.kch <magic.kch@yandex.ru>
2024-08-07 20:29:11 +0300

    Переработал методы под нужный вывод

--- VK_bot_old.py
+++ VK_bot_old.py
@@ -1,6 +1,7 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from VK_class import My_VkApi
+import json
 
 def write_msg(user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': 0})
@@ -26,12 +27,18 @@ for event in longpoll.listen():
         if event.to_me:
             vk_user = My_VkApi(group_access_token).get_user(event.user_id)
             print(vk_user, event.text)
+            sex = 1  # женский
+            age_from = 18
+            age_to = 50
+            city = vk_user[event.user_id]['city']
+
             # Сообщение от пользователя
             request = event.text
 
             # Каменная логика ответа
             if request == "привет":
-                write_msg(event.user_id, "Хай")
+                write_msg(event.user_id, "Хай, вот новая порция для вас!")
+                write_msg(event.user_id, My_VkApi(access_token).search_users(sex, age_from, age_to, city))
             elif request == "пока":
                 write_msg(event.user_id, "Пока((")
             else:
--- VK_class.py
+++ VK_class.py
@@ -1,9 +1,7 @@
 import requests
 import json
 from pprint import pprint
-
-import vk_api
-
+import datetime
 
 class HttpException(Exception):
     """Класс исключения, выбрасываем, когда API возвращает ошибку"""
@@ -51,8 +49,9 @@ class My_VkApi(ApiBasic):
                         'access_token': token,
                         'v': '5.199'
                         }
+        self.user_token = token
 
-    def get_user(self, user_id):
+    def get_user_info(self, user_id):
         """Получаем пользователя, используя унаследованный метод _send_request"""
         user_info = dict()
         user_info_resp = self._send_request(http_method='GET',
@@ -63,10 +62,12 @@ class My_VkApi(ApiBasic):
                                           },
                                   response_type='json'
                                   )
+        age_user = datetime.datetime.now().year - int(user_info_resp['response'][0]['bdate'].split('.')[2])
+
         user_info[user_id] = {"name": user_info_resp['response'][0]['first_name'],
                               "lastname": user_info_resp['response'][0]['last_name'],
                               "city": user_info_resp['response'][0]['city']['title'],
-                              "bdate": user_info_resp['response'][0]['bdate'],
+                              "age": age_user,
                               "sex": 'Женский' if user_info_resp['response'][0]['sex'] == 1 else 'Мужской'
                               if user_info_resp['response'][0]['sex'] == 2 else 'Неизвестный'
                               }
@@ -85,7 +86,7 @@ class My_VkApi(ApiBasic):
 
     def get_top3_likes(self, all_foto: dict):
         foto_count = 3
-        list_foto = dict()
+        list_foto = list()
 
         if all_foto['response']['count'] == 0:
             return "фотографии нет"
@@ -93,35 +94,44 @@ class My_VkApi(ApiBasic):
             foto_count = all_foto['response']['count']
 
         for i in all_foto['response']['items']:
-            list_foto[i['id']] = {"url": i['sizes'][-1]['url'], "likes": i['likes']['count']}
-
-        return sorted(list_foto.items(), key=lambda x: x[1]['likes'], reverse=True)[:foto_count]
+            # list_foto[i['id']] = {"owner_id": i['owner_id'], "likes": i['likes']['count']}
+            list_foto.append([f'photo{i["owner_id"]}_{i["id"]}', i['likes']['count']])
+        return sorted(list_foto, key=lambda x: x[1], reverse=True)[:foto_count]
     
+
     def search_users(self, sex, age_from, age_to, city):
+
+        return self._send_request(http_method='GET',
+                                  uri_path='method/users.search',
+                                  params={'sort': 1, # Параметр, отвечающий за сортировку результатов. Значение `1` означает, что результаты сортируются по релевантности.
+                                        'sex': sex, # Пол искомых пользователей. `1` — женский, `2` — мужской, `0` — не указывать пол.
+                                        'status': 1, # Семейное положение. Значение `1` означает, что пользователи должны быть "неженаты" (то есть, ищем тех, кто свободен)
+                                        'age_from': age_from, # Минимальный возраст пользователей (например, `18`).
+                                        'age_to': age_to, # Максимальный возраст пользователей (например, `30`).
+                                        'has_photo': 1, # Указывает, должны ли искомые пользователи иметь фотографии. Значение `1` ищет пользователей с фото.
+                                        'count': 3, # Количество возвращаемых результатов (например, `3` — возвращать 3 пользователей).
+                                        'online': 1, # Указывает, должны ли пользователи быть онлайн в данный момент. Значение `1` ищет только тех, кто в сети.
+                                        'hometown': city, # Город, в котором должны находиться искомые пользователи (например, название города).
+                                          **self.params
+                                          },
+                                  response_type='json'
+                                  )
+
+    def find_users_photos(self, find_users: dict):
         all_persons = []
-        link_profile = 'https://vk.com/id'
-        vk_ = vk_api.VkApi(token=user_token)
-        response = vk_.method('users.search',
-                            {'sort': 1, # Параметр, отвечающий за сортировку результатов. Значение `1` означает, что результаты сортируются по релевантности.
-                            'sex': sex, # Пол искомых пользователей. `1` — женский, `2` — мужской, `0` — не указывать пол.
-                            'status': 1, # Семейное положение. Значение `1` означает, что пользователи должны быть "неженаты" (то есть, ищем тех, кто свободен)
-                            'age_from': age_from, # Минимальный возраст пользователей (например, `18`).
-                            'age_to': age_to, # Максимальный возраст пользователей (например, `30`).
-                            'has_photo': 1, # Указывает, должны ли искомые пользователи иметь фотографии. Значение `1` ищет пользователей с фото.
-                            'count': 3, # Количество возвращаемых результатов (например, `3` — возвращать 3 пользователей).
-                            'online': 1, # Указывает, должны ли пользователи быть онлайн в данный момент. Значение `1` ищет только тех, кто в сети.
-                            'hometown': city # Город, в котором должны находиться искомые пользователи (например, название города).
-                            })
-        for element in response['items']:
-            all_foto = dict(vk.get_user_photos(element['id'])) # Получаем все фото пользователя
+
+        for element in find_users['response']['items']:
+            all_foto = dict(self.get_user_photos(element['id']))  # Получаем все фото пользователя
             person = [
                 element['first_name'],
                 element['last_name'],
-                link_profile + str(element['id']),
-                vk.get_top3_likes(all_foto) # Получаем топ 3 фото по количеству лайков
+                'https://vk.com/id' + str(element['id']),
+                self.get_top3_likes(all_foto)  # Получаем топ 3 фото по количеству лайков
             ]
             all_persons.append(person)
+
         return all_persons
+
 
 if __name__ == '__main__':
 
@@ -132,17 +142,15 @@ if __name__ == '__main__':
         user_token = data_json["access_token"]
 
     vk = My_VkApi(group_access_token)
-    vk_user = vk.get_user(user_id)
+    vk_user = vk.get_user_info(user_id)
     pprint(vk_user)
+    age_user = vk_user[user_id]['age']
+    sex_user = vk_user[user_id]['sex']
 
-    # all_foto = dict(vk.get_user_photos(user_id))
-    # pprint(vk.get_top3_likes(all_foto))
-    # pprint(all_foto)
-
-
-    sex = 1 # женский
-    age_from = 18
-    age_to = 50
+    # pprint(vk.get_user_photos(user_id))
+    sex = 2 if sex_user == 'Женский' else 1 # женский
+    age_from = age_user - 10
+    age_to = age_user + 5
     city = vk_user[user_id]['city'] # город нашего пользователя
-
-    pprint(vk.search_users(sex, age_from, age_to, city))
+    find_users = vk.search_users(sex, age_from, age_to, city)
+    pprint(vk.find_users_photos(find_users))
