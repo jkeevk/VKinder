a8ac2e6501849de4100adec8a6d9740c1593d64c
Sergey Igumnov <jkeevk@yandex.ru>
2024-08-08 11:27:43 +0300

    Добавил внесение пользователя в БД при первом обращении. Добавил ответ бота на нажатие поиска пары

--- VK_bot.py
+++ VK_bot.py
@@ -3,6 +3,7 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from VK_class import My_VkApi
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 import json
+from db_tools import DB_creator, DB_editor
 
 
 def write_msg(user_id, message, keyboard=None, attachment=None):
@@ -20,6 +21,9 @@ def write_msg(user_id, message, keyboard=None, attachment=None):
 with open('token.json', 'r') as file:
     data_json = json.load(file)
     group_access_token = data_json["group_access_token"]
+    user_token = data_json["access_token"]
+
+    
 
 vk = vk_api.VkApi(token=group_access_token)
 
@@ -65,13 +69,29 @@ for event in longpoll.listen():
             age_to = age_user + 5
             city = vk_user[event.user_id]['city']
 
+            # создаём запись в базе данных
+            vk_user_id = next(iter(vk_user))
+            vk_user_sex = 2 if sex_user == 'Женский' else 1
+            print(vk_user_id, age_user, sex_user, city)
+            vk_user_db = DB_editor()
+            vk_user_db.register_user(vk_user_id, age_user, vk_user_sex, city)
+            
             # Сообщение от пользователя
             request = event.text
 
-
             # Логика ответа
             if request.lower() == "поиск пары":
-                write_msg(event.user_id, "Хай! Выбери одну из кнопок:", create_keyboard())
+                # Поиск людей
+                find_users = My_VkApi(user_token).search_users(sex, age_from, age_to, city)
+                find_photos = My_VkApi(user_token).find_users_photos(find_users)
+                target_name = find_photos[0][0]
+                target_last_name = find_photos[0][1]
+                target_url = find_photos[0][2]
+                target_attachments = [item for item in find_photos[0][3]]
+                write_msg(event.user_id, f'{target_name} {target_last_name}')
+                write_msg(event.user_id, target_url)
+                write_msg(event.user_id, '',attachment=f'{",".join(target_attachments)}')
+                write_msg(event.user_id, "Выбери одну из кнопок:", create_keyboard())
             elif request.lower() == "правила":
                 instructions = (
                             "Привет! Я бот, который поможет вам найти людей для знакомства в социальной сети ВКонтакте 🥰\n\n"
