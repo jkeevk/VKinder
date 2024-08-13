51722922cda616f4e52b32c5995f60dd3d23a0ad
Sergey Igumnov <jkeevk@yandex.ru>
2024-08-13 11:15:23 +0300

    Добавил возможность менять город

--- VK_bot_w_gTEST.py
+++ VK_bot_w_gTEST.py
@@ -37,6 +37,8 @@ def start_buttons():
     keyboard = VkKeyboard(one_time=True)  # Создаем клавиатуру
     keyboard.add_button('Поиск пары', color=VkKeyboardColor.POSITIVE)
     keyboard.add_button('Правила', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_line()  
+    keyboard.add_button('Сменить город', color=VkKeyboardColor.SECONDARY)
     return keyboard.get_keyboard()
 
 
@@ -69,30 +71,6 @@ def photo_generator(user_list: list):
         yield user_id  # берем пользователя из списка найденых
 
 
-def start_bot(user_id: int, found_city=None):
-    user_info = My_VkApi(group_access_token).get_user_info(user_id)
-    user_sex = user_info[user_id]['sex']
-    user_age = user_info[user_id]['age']
-    opposite_sex = 2 if user_sex == 'Женский' else 1  # выбираем противоположный пол
-    age_min = user_age - 10 if user_age - 10 >= 16 else 16  # выбираем минимальный возраст
-    age_max = user_age + 5
-    if found_city is None:
-        user_city = user_info[user_id].get('city', 'Неизвестен')  # Используем get для безопасного доступа
-    else:
-        user_city = found_city
-    # создаём запись в базе данных
-    user_vk_id = user_id  # Получаем ID пользователя
-    user_vk_sex = 2 if user_sex == 'Женский' else 1
-    user_database = DB_editor()  # Создание экземпляра редактора БД
-    user_database.register_user(user_vk_id, user_age, user_vk_sex, user_city)
-
-    all_found_users = My_VkApi(user_token).search_users(opposite_sex, age_min, age_max, user_city)
-    # создаем генератор
-    all_found_users_generator = photo_generator(all_found_users)
-
-    return user_vk_id, user_sex, user_age, opposite_sex, age_min, age_max, user_city, user_database, all_found_users_generator
-
-
 # Функция для генерации нового пользователя
 def next_found_user_message(user_id: int):
     # Проверяем что пользователь не находится в black list
@@ -116,9 +94,7 @@ def next_found_user_message(user_id: int):
             return None
 
 # Инициализация состояний
-state_configed = False 
-city_unknown = True # Флаг обозначающий, что город еще не установлен
-found_city = None
+state_configed = False
 
 # Основной цикл
 for event in longpoll.listen():
@@ -126,75 +102,159 @@ for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         # Проверка, что сообщение адресовано боту
         if event.to_me:
+            user_info = My_VkApi(group_access_token).get_user_info(event.user_id)
+            user_sex = user_info[event.user_id]["sex"]
+            user_age = user_info[event.user_id]["age"]
+            opposite_sex = (
+                2 if user_sex == "Женский" else 1
+            )  # выбираем противоположный пол
+            age_min = (
+                user_age - 10 if user_age - 10 >= 16 else 16
+            )  # выбираем минимальный возраст
+            age_max = user_age + 5
+
+            user_vk_id = event.user_id  # Получаем ID пользователя
+            user_vk_sex = 2 if user_sex == "Женский" else 1
+            user_database = DB_editor()  # Создание экземпляра редактора БД
+
+            if not user_database.get_user_city(event.user_id):
+                user_city = user_info[event.user_id].get(
+                    "city", "Неизвестен"
+                )  # Используем get для безопасного доступа
+            else:
+                user_city = user_database.get_user_city(event.user_id)
+
+            user_database.register_user(user_vk_id, user_age, user_vk_sex, user_city)
             # Если город известен, продолжаем обработку
-            if not city_unknown:
+            if not user_database.get_user_city(event.user_id) == "Неизвестен":
                 # Если бот еще не настроен для пользователя
-                if state_configed == False:
+                if state_configed is False:
                     # Инициализация бота для текущего пользователя
-                    user_vk_id, user_sex, user_age, opposite_sex, age_min, age_max, user_city, user_database, all_found_users_generator = start_bot(event.user_id, found_city)
+                    user_city = user_database.get_user_city(
+                        event.user_id
+                    )
+                    all_found_users = My_VkApi(user_token).search_users(
+                        opposite_sex, age_min, age_max, user_city
+                    )
+                    # создаем генератор
+                    all_found_users_generator = photo_generator(all_found_users)
                     state_configed = True  # Пользователь теперь настроен
-                if state_configed:                
-                    user_request = event.text.lower() 
-                # Обработка сообщения от пользователя
-                # Логика обработки пользовательских запросов
+
+                if state_configed:
+                    user_request = event.text.lower()
+                    # Обработка сообщения от пользователя
                     if user_request == "поиск пары":
                         try:
                             # Получаем следующего пользователя для отображения
-                            user_id, found_user_fio, top3_user_photos = next_found_user_message(next(all_found_users_generator))
+                            (
+                                user_id,
+                                found_user_fio,
+                                top3_user_photos,
+                            ) = next_found_user_message(next(all_found_users_generator))
                         except TypeError:
                             write_msg(event.user_id, "Больше нет доступных фотографий.")
 
                     elif user_request == "правила":
                         write_msg(event.user_id, instructions, start_buttons())
 
+                    elif user_request == "сменить город":
+                        # сбрасываем город пользователя
+                        user_database.update_user_city(event.user_id, "Неизвестен")
+                        write_msg(event.user_id, "Укажите ваш город", start_buttons())
+
                     elif user_request == "пропустить":
                         try:
                             # Пропускаем текущего пользователя и получаем следующего
-                            user_id, found_user_fio, top3_user_photos = next_found_user_message(next(all_found_users_generator))
+                            (
+                                user_id,
+                                found_user_fio,
+                                top3_user_photos,
+                            ) = next_found_user_message(next(all_found_users_generator))
                         except StopIteration:
                             write_msg(event.user_id, "Больше нет доступных фотографий.")
-                            is_state_configured = False  # Сбрасываем настройку пользователя
-                            write_msg(event.user_id, "Возвращаемся в главное меню", start_buttons())
+                            state_configed = False  # Сбрасываем настройку пользователя
+                            write_msg(
+                                event.user_id,
+                                "Возвращаемся в главное меню",
+                                start_buttons(),
+                            )
 
                     elif user_request == "добавить в избранное":
-                        write_msg(event.user_id, f"https://vk.com/id{user_id}\nЗапись добавлена в избранное", create_keyboard())
+                        write_msg(
+                            event.user_id,
+                            f"https://vk.com/id{user_id}\nЗапись добавлена в избранное",
+                            create_keyboard(),
+                        )
                         user_database.add_to_favourites(
-                            user_vk_id, 
+                            user_vk_id,
                             found_user_fio.split()[0],
                             found_user_fio.split()[1],
-                            f'https://vk.com/id{user_id}',
-                            '{' + ''.join(top3_user_photos.split()) + '}'
+                            user_id,
+                            "{" + "".join(top3_user_photos.split()) + "}",
                         )
                         # Получаем следующего пользователя после добавления в избранное
-                        user_id, found_user_fio, top3_user_photos = next_found_user_message(next(all_found_users_generator))
+                        (
+                            user_id,
+                            found_user_fio,
+                            top3_user_photos,
+                        ) = next_found_user_message(next(all_found_users_generator))
 
                     elif user_request == "добавить в чёрный список":
-                        write_msg(event.user_id, f"https://vk.com/id{user_id}\nЗапись добавлена в чёрный список", create_keyboard())
+                        write_msg(
+                            event.user_id,
+                            f"https://vk.com/id{user_id}\nЗапись добавлена в чёрный список",
+                            create_keyboard(),
+                        )
                         user_database.add_to_black_list(user_vk_id, user_id)
                         # Получаем следующего пользователя после добавления в черный список
-                        user_id, found_user_fio, top3_user_photos = next_found_user_message(next(all_found_users_generator))
+                        (
+                            user_id,
+                            found_user_fio,
+                            top3_user_photos,
+                        ) = next_found_user_message(next(all_found_users_generator))
 
                     elif user_request == "просмотреть избранное":
                         user_favourites = user_database.get_favourites(user_vk_id)
-                        favourites_list = [f"{favourite['name']} {favourite['last_name']}: {favourite['url']}" for favourite in user_favourites]
-                        favourites_message = '\n'.join(favourites_list)
-                        write_msg(event.user_id, f'Ваш список избранного:\n\n{favourites_message}', start_buttons())
+                        favourites_list = [
+                            f"{favourite['name']} {favourite['last_name']}: https://vk.com/id{favourite['url']}"
+                            for favourite in user_favourites
+                        ]
+                        favourites_message = "\n".join(favourites_list)
+                        write_msg(
+                            event.user_id,
+                            f"Ваш список избранного:\n\n{favourites_message}",
+                            start_buttons(),
+                        )
 
                     elif user_request == "вернуться в главное меню":
-                        write_msg(event.user_id, "Возвращаемся в главное меню", start_buttons())
+                        write_msg(
+                            event.user_id,
+                            "Возвращаемся в главное меню",
+                            start_buttons(),
+                        )
 
                     else:
-                        write_msg(event.user_id, "Не поняла вашего ответа... Выбери одну из кнопок:", start_buttons())
+                        write_msg(
+                            event.user_id,
+                            "Не поняла вашего ответа... Выберите одну из кнопок:",
+                            start_buttons(),
+                        )
 
-            # Если город еще неизвестен
-            elif city_unknown:
+            # Если город еще неизвестен или находимся в состоянии смены города
+            elif user_database.get_user_city(event.user_id) == "Неизвестен":
                 user_request = event.text
-                found_city = My_VkApi(access_token).search_city(user_request) 
-                if user_request.lower() == "правила": 
-                    write_msg(event.user_id, instructions, start_buttons()) 
-                elif found_city == 'Город не найден':
-                    write_msg(event.user_id, "Укажите ваш город", start_buttons()) 
-                elif not found_city == 'Город не найден': 
-                    write_msg(event.user_id, f"Выбран город: {found_city}", start_buttons()) 
-                    city_unknown = False 
-                    continue
+                found_city = My_VkApi(access_token).search_city(user_request)
+                if user_request.lower() == "правила":
+                    write_msg(event.user_id, instructions, start_buttons())
+                elif found_city == "Город не найден":
+                    write_msg(event.user_id, "Укажите ваш город", start_buttons())
+                elif not found_city == "Город не найден":
+                    write_msg(
+                        event.user_id, f"Выбран город: {found_city}", start_buttons()
+                    )
+                    user_database.update_user_city(event.user_id, found_city)
+                    state_configed = False  # Сбрасываем настройку пользователя для генерации для нового города
+                    continue  # Если город выбран - выходим из цикла
+            # Если ввели что угодно
+            else:
+                write_msg(event.user_id, "Не поняла вашего ответа...", start_buttons())
